ba874f80957f9b7bf6771e78d1514807c4c66cc3
nayana1507 <nairnayana6@gmail.com>
2026-02-08 23:38:23 +0530

    initial commit

--- server/index.ts
+++ server/index.ts
@@ -91,7 +91,9 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || "5000", 10);
-  httpServer.listen(port, () => {
-  log(`serving on port ${port}`);
+
+httpServer.listen(port, () => {
+  const url = `http://localhost:${port}`;
+  log(`Server running at ${url}`);
 });
 })();
